6cb53cd7f5a57dc777a459360a975fa7fdded698
Hrvoje Labor <hrvoje.labor@ericsson.com>
2022-09-14 14:16:12 +0200

    Added Postgres

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+  """Add x and y and return result."""
+  return x + y
+
+def subtract(x, y):
+  """ Subtracts x and y and returns result. """
+  return y - x
