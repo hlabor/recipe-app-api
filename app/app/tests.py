73bc6d23b533424d93cad02caee6f4454366df6a
Hrvoje Labor <hrvoje.labor@ericsson.com>
2022-09-15 15:00:06 +0200

    Added Postgres (8)

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,9 +6,9 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """ Test the calc module."""
-
 
     def test_add_numbers(self):
         """Test adding numbers together."""
