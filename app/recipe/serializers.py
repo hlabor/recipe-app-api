9cb440f95787c8122fd8ab223e64fedc049a8b4c
Hrvoje Labor <hrvoje.labor@ericsson.com>
2023-02-23 10:52:30 +0100

    Ingredients (3)

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -35,7 +35,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 
+        fields = ['id', 'title', 'time_minutes', 'price', 'link',
                   'tags', 'ingredients', ]
         read_only_fields = ['id']
 
