9adb3fa482cbe932eda1770c9455ace67646da6a
Hrvoje Labor <hrvoje.labor@ericsson.com>
2023-02-27 15:41:58 +0100

    Recipe image API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -36,7 +36,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link',
-                  'tags', 'ingredients', ]
+                  'tags', 'ingredients', 'image', ]
         read_only_fields = ['id']
 
     def _get_or_create_tags(self, tags, recipe):
@@ -91,4 +91,14 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': True}}
